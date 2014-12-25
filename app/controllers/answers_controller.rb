e29d88bf481a759eafbd019139702e23b153f500
ds007007 <ds007007@yandex.ru>
2014-12-25 14:36:41 +0400

    Починено добавление и редактирование ответов, добавлена валидация параметров

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -28,7 +28,7 @@ class AnswersController < ApplicationController
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+        format.html { redirect_to @answer, notice: 'Ответ был успешно создан' }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class AnswersController < ApplicationController
   def update
     respond_to do |format|
       if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
+        format.html { redirect_to answers_url, notice: 'Ответ был успешно изменен' }
         format.json { render :show, status: :ok, location: @answer }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class AnswersController < ApplicationController
   def destroy
     @answer.destroy
     respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
+      format.html { redirect_to answers_url, notice: 'Ответ был успешно удален' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +69,6 @@ class AnswersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def answer_params
-      params.require(:answer).permit(:content)
+      params.require(:answer).permit(:content, :question_id, :is_correct)
     end
 end
